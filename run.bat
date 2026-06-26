6a4d86272a60123593e0a7ff7cef7f5146946565
JAANULO <aton.andrzejewski@gmail.com>
2026-06-26 16:01:37 +0200

    feat: dodanie portfela notatek akademickich i autostartu przegladarki

--- run.bat
+++ run.bat
@@ -1,5 +1,7 @@
 @echo off
 echo Uruchamianie serwera frontendowego na porcie 8000...
-echo Otworz w przegladarce: http://localhost:8000
+echo Otwieranie przegladarki: http://localhost:8000
+start http://localhost:8000
 python -m http.server 8000
 pause
+
